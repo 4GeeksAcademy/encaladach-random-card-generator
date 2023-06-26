d4925c3bcd91868680a5d61548799907cf8a4050
encaladach <119975587+encaladach@users.noreply.github.com>
2023-06-26 03:05:23 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.lenght);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.lenght);
+  return suit[indexSuit];
 };
