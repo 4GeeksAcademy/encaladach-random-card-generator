5de559265d6271f68864cc52a6fe0f71509dbf68
encaladach <119975587+encaladach@users.noreply.github.com>
2023-06-28 21:35:55 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
-window.onload = () => {
-  document.querySelector(".card").classList.add(generateRandomSuit());
-  document.querySelector(".card").innerHTML = generateRandomNumber();
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+
+  // 1.Button;
+  button();
+  // 2.cardGenerator
+  cardGenerator();
 };
 
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
+let button = () => {
+  let getNewCard = document.querySelector("#cardChanger");
+  console.log("testing");
+  getNewCard.addEventListener("click", function() {
+    cardGenerator();
+  });
+};
+let cardGenerator = () => {
+  // let cardGenerator =document.querySelector()
+  // 3. randomSuit =
+  let randomSuit = ["♦", "♣", "♥", "♠"];
+  // 4. randomNumber =
+  let randomNumber = [
     "2",
     "3",
     "4",
@@ -22,16 +37,24 @@ let generateRandomNumber = () => {
     "8",
     "9",
     "10",
-    "J",
+    "K",
     "Q",
-    "K"
+    "J",
+    "A"
   ];
-  let indexNumbers = Math.floor(Math.random() * numbers.lenght);
-  return numbers[indexNumbers];
-};
 
-let generateRandomSuit = () => {
-  let suit = ["diamond", "spade", "heart", "club"];
-  let indexSuit = Math.floor(Math.random() * suit.lenght);
-  return suit[indexSuit];
+  let suit = randomSuit[Math.floor(Math.random() * randomSuit.length)];
+  let number = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+
+  if (suit == "♥" || suit == "♦") {
+    document.querySelector("#top-icon").style.color = "red";
+    document.querySelector("#bottom-icon").style.color = "red";
+  } else {
+    document.querySelector("#top-icon").style.color = "black";
+    document.querySelector("#bottom-icon").style.color = "black";
+  }
+
+  document.querySelector("#top-icon").innerHTML = suit;
+  document.querySelector("#number").innerHTML = number;
+  document.querySelector("#bottom-icon").innerHTML = suit;
 };
